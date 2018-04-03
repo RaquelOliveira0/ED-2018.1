542afe7116429b9c3bead6b20e8d01a73bf554d7
RaquelOliveira0 <racheloliveira07@gmail.com>
2018-04-03 16:09:19 -0300

    Add files via upload

--- Princesa/main.cpp
+++ Princesa/main.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
 #include <list>
 using namespace std ;
+void salto(std::list<int> &lista,int saltos){
+    if(saltos == 2 ){
+       lista.push_back(lista.front());
+       lista.pop_front();
+        lista.pop_front();
+        lista.pop_front();
 
+        }
+
+     if(saltos == 3){
+       lista.push_back(lista.front());
+       lista.pop_front();
+        lista.pop_front();
+         lista.pop_front();
+         lista.pop_front();
+
+}else if(saltos == 5){
+        lista.push_back(lista.front());
+        lista.pop_front();
+         lista.pop_front();
+          lista.pop_front();
+          lista.pop_front();
+          lista.pop_front();
+          lista.pop_front();
+
+}
+    }
+
+
+
+void rodar(std::list<int> &lista){
+    lista.push_back(lista.front());
+    lista.pop_front();
+}
 void mostrarLista(list <int> lista){
  int size = lista.size();
     for(int x = 0; x < size; x++){
@@ -13,25 +46,35 @@ void mostrarLista(list <int> lista){
 int main() {
 int qtd= 0;
 int escolha = 0;
-
+int saltos=0;
+cout<<"Digite a quntidade e a escolha"<<"\n";
 cin >> qtd >> escolha;
-
+cout << "Digite o numero de saltos : 0 ,2, 3 ou 5"<<"\n";
+cin >> saltos;
 list <int> lista;
 for(int i = 1;i <=qtd ; i++){
   lista.push_back(i);
 }
+ while(lista.front()!= escolha ){
+    rodar(lista);
+   //lista.push_back(lista.front());
+   // lista.pop_front();
 
-  while(lista.front()!= escolha ){
-    lista.push_back(lista.front());
-    lista.pop_front();
-  }
-  while(lista.size()!= 1){
+ }
     mostrarLista(lista);
-    lista.push_back(lista.front());
-    lista.pop_front();
-    lista.pop_front();
+  while(lista.size() > 1 ) {
+      if(saltos == 0){
+      rodar(lista);
+      lista.pop_front();
+      mostrarLista(lista);
+      }else if(saltos > 0){
+       salto(lista,saltos);
+       mostrarLista(lista);
+
+      }
+
   }
-   mostrarLista(lista);
+
 
 return 0;
 
